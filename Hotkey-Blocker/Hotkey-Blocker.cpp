446b2ce235b98a7d74bcfc465ae5267e529d72c0
Andy Roxby <roxby@production.pro>
2015-04-01 22:47:03 -0400

    Restructure hotkey processing

--- Hotkey-Blocker/Hotkey-Blocker.cpp
+++ Hotkey-Blocker/Hotkey-Blocker.cpp
@@ -6,6 +6,17 @@
 #include <windows.h>
 using namespace std;
 
+bool keepGoing = true;
+int cappedKeys[] =
+{
+        VK_DIVIDE, /* Exit Key */
+        VK_F1,
+        VK_F2,
+        VK_F3,
+        VK_F4
+};
+const static auto arraySize = sizeof(cappedKeys) / sizeof(int);
+
 /* This is our message callback */
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -28,17 +39,6 @@ int main()
     }
 
 	SetConsoleTitle("Hotkey Blocker");
-	bool keepGoing = true;
-
-	int cappedKeys[] =
-	{
-		VK_DIVIDE, /* Exit Key */
-		VK_F1,
-		VK_F2,
-		VK_F3,
-		VK_F4
-	};
-	const static auto arraySize = sizeof(cappedKeys) / sizeof(int);
 	printf( "To exit press Num Pad Divide.\n" ); /* Why doesn't this key get passed? */
 
 	bool errorKey = false;
@@ -48,38 +48,41 @@ int main()
 		{
 			errorKey = true;
 			printf( "Something went wrong with hotkey #%d!\n", i );
+                        /* Why not terminate the program here? */
 		}
 	}
 	if( !errorKey )
 		printf( "Ready to rock and roll!\n" );
 
 	/* Main message loop */
-	MSG msg = {0};
+	MSG msg;
 	do
 	{
-        if( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) != 0 )
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-			if (msg.message == WM_HOTKEY)
-			{
-				int daKey = msg.wParam;
-				if (daKey == 0 )
-					keepGoing = false;
-				HWND h = GetForegroundWindow();
-				PostMessage( h, WM_KEYDOWN, cappedKeys[daKey], 0 );
-				PostMessage( h, WM_KEYUP, cappedKeys[daKey], 0 );
-			}
-		}
-		Sleep(10);
+            if( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) != 0 )
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+	    Sleep(10);
 	}while(keepGoing);
     return msg.wParam;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+    int daKey;
+    HWND fgWindow;
+
     switch (message)
     {
+    case WM_HOTKEY:
+        daKey = msg.wParam;
+        if (daKey == 0 )
+                keepGoing = false;
+        fgWindow = GetForegroundWindow();
+        PostMessage( h, WM_KEYDOWN, cappedKeys[daKey], 0 );
+        PostMessage( h, WM_KEYUP, cappedKeys[daKey], 0 );
+        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
